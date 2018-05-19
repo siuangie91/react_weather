502f1cec72854096e7eba998d8cd6d4fb464efa1
siuangie91 <siuangie91@gmail.com>
2018-05-19 00:29:24 -0400

    format results in DatesContainer

--- src/_components/DatesContainer.js
+++ src/_components/DatesContainer.js
@@ -28,6 +28,38 @@ const formatDate = function(dateString) {
 	return formattedDate;
 };
 
+const formatTime = function(timeIn24HrFormat) {
+	const splitTime = timeIn24HrFormat.split(':');
+
+	const hour = splitTime[0];
+	const minutes = splitTime[1];
+
+	let formmattedHour;
+	let dayNight;
+
+	if(+hour <= 11) { // use "+" to force as a number
+		if(+hour <= 10) {
+			if(+hour == 0) {
+				formmattedHour = "12";
+			} else {
+				formmattedHour = hour[1]; // don't take the beginning zero
+			}
+		} else {
+			formmattedHour = hour;
+		}
+
+		dayNight = "AM";
+	} else {
+		formmattedHour = +hour - 12;
+		if(formmattedHour == 0) {
+			formmattedHour = "12";
+		}
+		dayNight = "PM";
+	}
+
+	return `${formmattedHour}:${minutes} ${dayNight}`
+}
+
 export default class DatesContainer extends React.Component {
 	render() {
 		const {city, country, results} = this.props;
@@ -41,6 +73,18 @@ export default class DatesContainer extends React.Component {
 						return (
 							<div className="date" key={i}>
 								<h2>{formatDate(result.name)}</h2>
+								<ul>
+									{
+										result.weathers.map((weatherItem, i) => {
+											return (
+												<li key={i}>
+													<strong>{formatTime(weatherItem.dt_txt.split(' ')[1])}</strong> &mdash; 
+													{weatherItem.weather[0].main}: {weatherItem.weather[0].description}
+												</li>
+											);
+										})
+									}
+								</ul>
 							</div>	
 						)
 					})
